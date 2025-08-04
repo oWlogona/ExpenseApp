3f4ddfb2ba1a1368fd2fcb5464b885c809211d86
Andrew <artemdanilenko1997@gmail.com>
2025-08-04 15:21:11 +0300

    init: Expense project and base logic

---
+++ components/ExpensesOutput.js
@@ -0,0 +1,24 @@
+import { StyleSheet, View } from "react-native";
+
+import { GlobalStyles } from "../constants/styles";
+import ExpensesList from "./ExpensesList";
+import ExpensesSummary from "./ExpensesSummary";
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
+  return (
+    <View style={styles.container}>
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesList expenses={expenses} />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
